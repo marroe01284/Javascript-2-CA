87d175d22e1fd58c9c40cee92d1958a41ece624d
TOMCHRISTER\tomch <tomsch01266@stud.noroff.no>
2024-09-25 11:57:55 +0200

    added register user

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,8 +1,34 @@
-export async function register({
-  name,
-  email,
-  password,
-  bio,
-  banner,
-  avatar,
-}) {}
+import {API_AUTH_REGISTER} from "../constants.js";
+
+export async function register({ name, email, password, bio, banner, avatar }) {
+  const headers = new Headers({
+    'Content-Type': 'application/json',
+  });
+
+  const requiredData = { name, email, password };
+
+  if (bio) requiredData.bio = bio;
+  if (banner) requiredData.banner = banner;
+  if (avatar) requiredData.avatar = avatar;
+
+  const requestOptions = {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(requiredData)
+  };
+
+  try {
+    const response = await fetch(API_AUTH_REGISTER, requestOptions);
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      console.error('Registration failed.', errorResponse);
+      return {error: errorResponse.error || 'Registration failed.'};
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error('Error during registration:', error);
+    return {error: error.message};
+  }
+}
