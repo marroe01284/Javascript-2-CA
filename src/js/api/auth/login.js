a729dc3569e1b301b4ccb7fa4a691c859e84daa0
TOMCHRISTER\tomch <tomsch01266@stud.noroff.no>
2024-09-25 11:18:09 +0200

    added login user

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,29 @@
+import {API_AUTH_LOGIN, API_KEY} from "../constants.js";
+
 export async function login({ email, password }) {}
+
+export async function apiLogin({ email, password }) {
+    try {
+        const response = await fetch(API_AUTH_LOGIN, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Noroff-API-Key': API_KEY,
+            },
+            body: JSON.stringify({ email, password }),
+        });
+
+        if (!response.ok) {
+            return {error: 'Login failed.'};
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        return {error: error.message};
+    }
+}
+
+
+/*const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVG9tX0NocmlzdGVyX1MiLCJlbWFpbCI6InRvbWNocmlzdGVyMTk5NUBzdHVkLm5vcm9mZi5ubyIsImlhdCI6MTcyNzE3MDA4Mn0.SxibC693N--JpYxn_Mf3zkajhI1PBzYQdMp3RcHWx3Q';
+
+const apiKey = 'be9052e7-bb3f-4b31-88fd-80fe785919c7'*/
--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,19 @@
-export async function onLogin(event) {}
+import { apiLogin } from '../../api/auth/login.js';
+
+export async function onLogin(event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const result = await apiLogin({ email, password });
+
+    if (result.error) {
+        alert("Login failed: " + result.error);
+    } else {
+        localStorage.setItem("accessToken", result.data.accessToken);
+        localStorage.setItem("userID", result.data.name);
+
+        window.location.href = "/";
+    }
+}
