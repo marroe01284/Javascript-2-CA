bc3d1da09404ff0102efa565ac1350c9ccd9abc2
Marius Rønning <mpr941@hotmail.com>
2024-09-25 11:02:31 +0200

    Feat: updatetd the getKey function retreives the accsesstoken from localstorage

--- src/js/api/auth/key.js
+++ src/js/api/auth/key.js
@@ -1,1 +1,8 @@
-export async function getKey(name) {}
+export async function getKey() {
+    const accsessToken = localStorage.getItem('accsessToken');
+    if (!accessToken) {
+            console.log('No access token found');
+            return null;
+        }
+        return accessToken;
+}
