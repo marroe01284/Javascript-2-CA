822f00228228dac09b7cff0d185fee5a2fa08088
Marius Rønning <mpr941@hotmail.com>
2024-09-25 12:46:20 +0200

    Feat: API: createPost function sends JSON data + token authorization creates data for the post rewuest options sneds the rwuest handle response throws errors if respons is ok or not.

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,1 +1,17 @@
-export async function onCreatePost(event) {}
+import { createPost } from "../../api/post/create";
+export async function onCreatePost(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const title = form.title.value;
+    const body = form.post.value; 
+    const media = form.image.value;
+    const tags = form.tags.value; 
+  
+    try {
+      await createPost({ title, body, tags, media });
+      console.log("Post created successfully!");
+    } catch (error) {
+      console.error("Error creating post:", error);
+    }
+}
